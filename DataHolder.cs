d903aa2db1f3d5b5949f953d3ab7f6027d38fb12
Israr H <32361952+IsrarHussain99@users.noreply.github.com>
2019-08-13 23:20:10 +0100

    Add files via upload

---
+++ DataHolder.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using NUnit.Framework;
+
+public class DataHolder : MonoBehaviour {
+
+public List<string> EngineNames;
+public int[] EngineSpeeds;
+public AudioClip[] EngineSounds;
+
+//public CubeMap[] CubeMaps;
+
+public GameObject[] BatteryPreview;
+public GameObject[] SparkPlugsPreview;
+public GameObject[] AirFilterPreview;
+public GameObject[] GearBoxPreview;
+// public Texture Undrbtn;
+public GameObject[] FuelTankPreview;
+public GameObject[] EnginePreview;
+
+
+
+
+}
